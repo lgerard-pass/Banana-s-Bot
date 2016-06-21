9ba908851dcfdf89b43987c57a05bbbaf34dd8dc
Louis GERARD <louis.gerard@isen-lille.fr>
2016-06-21 11:07:28 +0200

    Commands for GO4 implemented

--- bananaBot.py
+++ bananaBot.py
@@ -4,7 +4,6 @@ from discord.ext import commands
 import datetime
 import random
 
-
 #Creating client
 description = "Bot for Banana's discord"
 client = commands.Bot(command_prefix='!', description=description)
@@ -25,7 +24,6 @@ async def on_ready():
 #-----------------------Defining bot commands-------------------------------------------------
 @client.command()
 async def roll(dice : str):
-    threshold = 200
     """Rolls a dice in NdN format."""
     try:
         rolls, limit = map(int, dice.split('d'))
@@ -38,6 +36,78 @@ async def roll(dice : str):
         result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
         await client.say(result)
 
+@client.command(pass_context=True)
+async def register(ctx,date : str):
+    """Format : DD-MM. Register for GO4 at given date"""
+    try:
+        member = ctx.message.author
+        day, month = map(int, date.split('-'))
+        my_date = datetime.date(datetime.datetime.now().year, month, day)
+        if my_date < datetime.date.today() or (my_date.weekday() != 2 and my_date.weekday() != 6):
+                raise ValueError()
+        f = open('GO4_' + str(my_date) + '.reg'  , 'a+')
+        position = f.seek(0,0)
+        content = f.read()
+        if member.name in content :
+                await client.say('Tu t\'es deja inscrit champion !')
+        else:
+                position = f.seek(0, 2)
+                f.write(member.name + ' ' + str(datetime.datetime.now()) + '\n')
+                await client.say('Inscription prise en compte')
+        f.close()
+    except ValueError:
+                await client.say('Erreur dans la date donnee') 
+
+@client.command(pass_context=True)
+async def unregister(ctx,date : str):
+    """Format : DD-MM. Unregister for GO4 at given date"""
+    try:
+        member = ctx.message.author
+        day, month = map(int, date.split('-'))
+        my_date = datetime.date(datetime.datetime.now().year, month, day)
+        if my_date < datetime.date.today() or (my_date.weekday() != 2 and my_date.weekday() != 6):
+                raise ValueError()
+        f = open('GO4_' + str(my_date) + '.reg'  , 'r')
+        f.seek(0,0)
+        lines = f.readlines()
+        found = False
+        for line in lines:
+                if(line.startswith(member.name)):
+                    lines.remove(line)
+                    found = True
+        f.close()
+        if not found :
+               raise LookupError()
+        f = open('GO4_' + str(my_date) + '.reg'  , 'w')
+        f.seek(0,0)
+        for line in lines:
+                f.write(line)
+        f.close()
+        await client.say('Desinscription prise en compte')
+    except ValueError:
+                await client.say('Erreur dans la date donnee')
+    except LookupError:
+                await client.say('Tu n\'etais pas inscrit a cette date')
+
+@client.command(pass_context=True)
+async def whoplays(ctx,date : str):
+    """Format : DD-MM. Says who registered for GO4 at given date"""
+    try:
+        day, month = map(int, date.split('-'))
+        my_date = datetime.date(datetime.datetime.now().year, month, day)
+        if my_date < datetime.date.today() or (my_date.weekday() != 2 and my_date.weekday() != 6):
+              raise ValueError()
+        f = open('GO4_' + str(my_date) + '.reg'  , 'r')
+        content = f.read()
+        if content == '':
+             raise FileNotFoundError()
+        await client.say(content)
+        f.close()
+    except ValueError:
+              await client.say('Erreur dans la date donnee')
+    except FileNotFoundError:
+              await client.say('Personne n\'est encore inscrit a cette date')
+
 
 #-----------------------Defining background tasks-------------------------------------------------
 #This task will run in background and will remind everyone to go training 1hour beforehand
@@ -47,7 +117,7 @@ async def check_training():
     while not client.is_closed:
         now = datetime.datetime.now()
         if now.weekday()==0 or now.weekday()==3:
-            if now.hour==19:
+            if now.hour==20:
                 role = discord.utils.find(lambda r: r.name == 'overwatch_players', client.get_server(bananasDiscordId).roles) 
                 message = role.mention + ' Yo mes ptits poulets, oubliez pas le training de ce soir de 21h a 23h, sinon Loulou il va raler !'
                 await client.send_message(channel, message)
@@ -59,7 +129,7 @@ async def check_training():
         await asyncio.sleep(3600) # task runs every hour
 
 #-----------------------Launching background task-------------------------------------------------
-#client.loop.create_task(check_training())
+client.loop.create_task(check_training())
 
 #-----------------------Connectiong client-------------------------------------------------
 client.run('MTkzNDk3MzA1MDEyMjQwMzg1.CkYP6A.dx6V_0bgCAsaa5GkRDr8ZEA6beg')
