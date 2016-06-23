baeba9e38bec7f71391452879bc7e3a9550e17a8
Louis Gérard <Louis Grard>
2016-06-24 00:02:20 +0200

    set game channel

--- client/bananaBot.py
+++ client/bananaBot.py
@@ -52,44 +52,45 @@ class bananaBot():
                 result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
                 await self.client.say(result)
 
-        @self.client.command()
-        async def hangman():
+        @self.client.command(pass_context=True)
+        async def hangman(ctx):
             """Launch a hangman game."""
-            if self.game_running:
-                await self.client.say("Une partie est déjà en cours, attendez la fin !")
-                return
-            self.game_running = True
-            remaining_tries = nb_tries
-            found_letters = []
-            word = selectWord().upper()
-            print(word)
-            revealedWord = get_revealed_word(word,found_letters)
-            print(revealedWord)
-            await self.client.say('Allez c\'est tipar')
-            won = False
-            lost = False
-            while not(won) and not(lost):
-                await self.client.say("```" + revealedWord + '\n' + "Tentatives restantes : "  + str(remaining_tries) + "```")
-                answer = await self.client.wait_for_message(timeout=10.0, check=hang_check)
-                if answer is None :
-                    await self.client.say("Vous avez été trop long(s), il faut répondre sous 10 secondes\nFin de la partie")
-                    self.game_running = False
+            if ctx.message.channel.id != game_channel_id:
+                if self.game_running:
+                    await self.client.say("Une partie est déjà en cours, attendez la fin !")
                     return
-                else:
-                    answer = answer.content.upper()
-                    if answer in found_letters:
-                        await self.client.say("Vous avez déjà choisi cette lettre.")
-                    elif answer in word:
-                        found_letters.append(answer)
+                self.game_running = True
+                remaining_tries = nb_tries
+                found_letters = []
+                word = selectWord().upper()
+                print(word)
+                revealedWord = get_revealed_word(word,found_letters)
+                print(revealedWord)
+                await self.client.say('Allez c\'est tipar')
+                won = False
+                lost = False
+                while not(won) and not(lost):
+                    await self.client.say("```" + revealedWord + '\n' + "Tentatives restantes : "  + str(remaining_tries) + "```")
+                    answer = await self.client.wait_for_message(timeout=10.0, check=hang_check)
+                    if answer is None :
+                        await self.client.say("Vous avez été trop long(s), il faut répondre sous 10 secondes\nFin de la partie")
+                        self.game_running = False
+                        return
                     else:
-                        remaining_tries -= 1
-                    revealedWord = get_revealed_word(word,found_letters)
-                won,lost = evaluate_game(word,revealedWord,remaining_tries)
-            if won :
-                await self.client.say("```Bien joué ! Le mot était : " + word + "```")
-            else :
-                await self.client.say("```Vous êtes mauvais ! Le mot était : " + word + "```")
-            self.game_running = False
+                        answer = answer.content.upper()
+                        if answer in found_letters:
+                            await self.client.say("Vous avez déjà choisi cette lettre.")
+                        elif answer in word:
+                            found_letters.append(answer)
+                        else:
+                            remaining_tries -= 1
+                        revealedWord = get_revealed_word(word,found_letters)
+                    won,lost = evaluate_game(word,revealedWord,remaining_tries)
+                if won :
+                    await self.client.say("```Bien joué ! Le mot était : " + word + "```")
+                else :
+                    await self.client.say("```Vous êtes mauvais ! Le mot était : " + word + "```")
+                self.game_running = False
 
         @self.client.command(pass_context=True)
         async def register(ctx, date: str):
--- client/constants.py
+++ client/constants.py
@@ -5,6 +5,7 @@ overwatchChannelId = '173417024825982976'
 adminId = '101590011652096000'
 token = 'MTk1NTEzNjMyMzI1NTAwOTI4.Ck1pGg.QXy7_6epx1q3ex-qSv3oXwjB0CQ'
 token_test = 'MTk1MjI2ODUwNzMyMTQ2Njg4.Ck2UmQ.ea5IBxu1P9XNogb2qptycQ781qk'
+game_channel_id = '195658673400446976'
 words = [
     "ANGLE",
     "ARMOIRE",
