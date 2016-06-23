9d5834c8b6207fa33b189f7e6b685208c8d2b360
Louis Gérard <Louis Grard>
2016-06-23 19:02:43 +0200

    Refactoring

---
+++ client/constants.py
@@ -0,0 +1,8 @@
+#Specific ID
+clientDesc = "Bot for Banana's discord"
+bananasDiscordId = '101591369981632512'
+overwatchChannelId = '173417024825982976'
+adminId = '101590011652096000'
+token = 'MTk1NTEzNjMyMzI1NTAwOTI4.Ck1pGg.QXy7_6epx1q3ex-qSv3oXwjB0CQ'
+token_test = 'MTk1MjI2ODUwNzMyMTQ2Njg4.Ck2UmQ.ea5IBxu1P9XNogb2qptycQ781qk'
+
---
+++ main.py
@@ -0,0 +1,5 @@
+from client.bananaBot import bananaBot
+from client.constants import *
+
+bot = bananaBot(token_test)
+bot.run()
