c2b8366d6e462aa5337edb709fbbd454368dae74
Louis GERARD <louis.gerard@isen-lille.fr>
2016-07-04 18:39:02 +0200

    music commands

--- client/cogs/music.py
+++ client/cogs/music.py
@@ -4,12 +4,15 @@ import datetime
 from client.util.util import parseDate
 import discord
 import asyncio
+from ctypes.util import find_library
 
 class Music:
     """Commands related to music playback"""
     def __init__(self, bot):
         self.bot = bot
         self.voice = None
+        self.player = None
+        discord.opus.load_opus(find_library("opus"))
 
     @commands.command(pass_context=True, no_pm=True)
     async def jointheparty(self, ctx, channelName : str):
@@ -19,6 +22,21 @@ class Music:
             if not(self.voice is None):
                 await self.voice.move_to(channel)
             self.voice = await self.bot.join_voice_channel(channel)
+                 
+		
+    @commands.command(no_pm=True)
+    async def play(self,link : str):
+        if not(self.voice is None):
+            self.player = await self.voice.create_ytdl_player(link)
+            self.player.start()
+        else:
+            await self.bot.reply("Connecte moi à un channel d\'abord")
+
+    @commands.command(no_pm=True)
+    async def stop(self):
+        if not(self.player is None):
+            self.player.stop()
+            self.player = None
 
     @commands.command(no_pm=True)
     async def leavetheparty(self):
